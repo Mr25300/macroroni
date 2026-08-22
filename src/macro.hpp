f1f53b9992fa84728bcb21c4eb74af9a8d9123e9
Sebastian Cooper-Giannotta <109829617+Mr25300@users.noreply.github.com>
2026-08-22 01:01:02 -0400

    feat: make macro run in a separate thread

--- src/macro.hpp
+++ src/macro.hpp
@@ -4,11 +4,13 @@
 #include <cstdint>
 #include <vector>
 #include <algorithm>
+#include <atomic>
+#include <thread>
 
 #include <iostream>
 
 struct MacroAction {
-    IOInfo out_info;
+    IOInfo output;
     uint64_t time_ms;
     uint32_t id{};
 };
@@ -19,8 +21,14 @@ class Macro {
     size_t loop_count{};
     uint64_t loop_delay{};
 
+public:
+    IOInfo start_trigger;
+    IOInfo stop_trigger;
+
+private:
     uint32_t next_id{};
-    bool running{};
+    std::thread run_thread;
+    std::atomic<bool> running{};
 
 public:
     Macro(IOController& io, size_t loops, uint64_t delay_ms, std::vector<MacroAction> init_actions)
@@ -35,7 +43,9 @@ public:
         }
     }
 
-    uint32_t add_action(MacroAction action) {
+    int32_t add_action(MacroAction action) {
+        if (running) return -1;
+
         std::vector<MacroAction>::iterator it = std::upper_bound(
             actions.begin(), actions.end(), action,
             [](const MacroAction& a, const MacroAction& b) {
@@ -49,6 +59,8 @@ public:
     }
 
     void remove_action(uint32_t action_id) {
+        if (running) return;
+
         std::vector<MacroAction>::iterator it = std::find_if(
             actions.begin(), actions.end(),
             [action_id](const MacroAction& a) {
@@ -59,71 +71,65 @@ public:
         if (it != actions.end()) actions.erase(it);
     }
 
-    void run() {
+    void start() {
         if (running) return;
+        if (run_thread.joinable()) run_thread.join();
 
         running = true;
 
-        size_t action_index{};
-        size_t loop_num{};
+        run_thread = std::thread{[this]() {
+            const uint64_t last_time = actions.back().time_ms;
+            const uint64_t loop_time = last_time + loop_delay;
 
-        uint64_t last_time = actions.back().time_ms;
-        uint64_t loop_time = last_time + loop_delay;
+            size_t action_index{};
+            size_t loop_num{};
 
-        run_timer(1000, [&](uint64_t time_ms) {
-            if (!running) return false;
+            run_timer(1000, [this, last_time, loop_time, &action_index, &loop_num](uint64_t time_ms) {
+                if (!running) return false;
 
-            while (true) {
-                uint64_t curr_time_ms = time_ms - loop_num * loop_time;
+                while (true) {
+                    uint64_t curr_time_ms = time_ms - loop_num * loop_time;
 
-                while (action_index < actions.size()) {
-                    MacroAction& action = actions[action_index];
+                    while (action_index < actions.size()) {
+                        MacroAction& action = actions[action_index];
 
-                    if (curr_time_ms >= action.time_ms) {
-                        std::cout << action.out_info.code << ' ' << action.out_info.value << '\n';
-                        // io_control.execute(action.out_info);
-                        ++action_index;
+                        if (curr_time_ms >= action.time_ms) {
+                            std::cout << action.output.code << ' ' << action.output.value << '\n';
+                            // io_control.execute(action.out_info);
+                            ++action_index;
+                        } else {
+                            break;
+                        }
+                    }
+
+                    if (
+                        curr_time_ms >= loop_time ||
+                        // Never true if loop_count is 0
+                        loop_num == loop_count - 1 && curr_time_ms >= last_time
+                    ) {
+                        ++loop_num;
+
+                        if (loop_count == 0 || loop_num < loop_count) {
+                            action_index = 0;
+                        } else {
+                            return false;
+                        }
                     } else {
                         break;
                     }
                 }
 
-                if (
-                    curr_time_ms >= loop_time ||
-                    loop_num == loop_count - 1 && curr_time_ms >= last_time
-                ) {
-                    ++loop_num;
+                return true;
+            });
 
-                    if (loop_count == 0 || loop_num < loop_count) {
-                        action_index = 0;
-                    } else {
-                        return false;
-                    }
-                } else {
-                    break;
-                }
-
-                // // TODO: This isn't the right condition if there is a delay time between loops
-                // if (action_index >= actions.size()) {
-                //     ++loop_num;
-                //
-                //     if (loop_count == 0 || loop_num < loop_count) {
-                //         action_index = 0;
-                //     } else {
-                //         return false;
-                //     }
-                // } else {
-                //     break;
-                // }
-            }
-
-            return true;
-        });
-
-        running = false;
+            running = false;
+        }};
     }
 
     void stop() {
+        if (!running) return;
         running = false;
+
+        if (run_thread.joinable()) run_thread.join();
     }
 };
